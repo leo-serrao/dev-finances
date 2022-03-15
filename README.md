9966119f3f98f043d436b83ab16002d97e83711e
Leonardo Serrão <leoserraom@gmail.com>
2022-03-15 17:14:29 -0300

    first functionalities added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# This is a finances system made during "Maratona Discover 1" from RocketSeat 
+# This is a finances system made during "Maratona Discover 1" from RocketSeat
+
 <h2> In this project i'll be using  <img alt="html-badge" src="https://img.shields.io/badge/HTML-239120?style=for-the-badge&logo=html5&logoColor=white" /> <img alt="css-badge" src="https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white" /> <img alt="javascript-badge" src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" /> </h2>
+
+<h3> This is a code made during studying sections and may have some comments in the codes <h3>
--- app/script.js
+++ app/script.js
@@ -1,14 +1,153 @@
+//----------  ABRE/FECHA O MODAL --------------//
 const modalBtn = document.querySelector('.button.new')
 const cancelBtn = document.querySelector('.button.cancel')
 const modal = document.querySelector('.modal-overlay')
 
-function openModal() {
-  modal.classList.add('active')
+function toggleModal() {
+  if (modal.classList === 'modal-overlay active') {
+    modal.classList.toggle('active')
+  } else {
+    modal.classList.toggle('active')
+  }
 }
 
-function closeModal() {
-  modal.classList.remove('active')
+modalBtn.addEventListener('click', toggleModal)
+cancelBtn.addEventListener('click', toggleModal)
+
+//----------  FUNCIONALIDADES DA PÁGINA --------------//
+
+//-----  Arr com as transações exibidas na tabela -----//
+const transactions = [
+  {
+    id: 1,
+    description: 'Luz',
+    amount: -50000,
+    date: '15/03/2022'
+  },
+  {
+    id: 2,
+    description: 'Website',
+    amount: 500000,
+    date: '15/03/2022'
+  },
+  {
+    id: 3,
+    description: 'Internet',
+    amount: -20000,
+    date: '15/03/2022'
+  }
+]
+
+//-----  Calculos de entrada, saída e total -----//
+const Transaction = {
+  //-----  Calcula as entradas -----//
+  incomes() {
+    let income = 0
+
+    transactions.forEach(transaction => {
+      if (transaction.amount > 0) {
+        income += transaction.amount
+      }
+    })
+
+    return income
+  },
+  //-----  Calcula as saídas -----//
+  expenses() {
+    let expense = 0
+
+    transactions.forEach(transaction => {
+      if (transaction.amount < 0) {
+        expense += transaction.amount
+      }
+    })
+
+    return expense
+  },
+  //-----  Calcula o total -----//
+  total() {
+    let total = 0
+
+    transactions.forEach(transaction => {
+      total = this.incomes() + this.expenses()
+    })
+
+    return total
+  }
 }
 
-modalBtn.addEventListener('click', openModal)
-cancelBtn.addEventListener('click', closeModal)
+//----- Funcionalidades ligadas ao DOM -----//
+const DOM = {
+  transactionsContainer: document.querySelector('#data-table tbody'),
+
+  //-----  Insere o elemento criado na tabela -----//
+  addTransaction(transaction) {
+    const tr = document.createElement('tr')
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+    DOM.transactionsContainer.appendChild(tr)
+  },
+
+  //-----  Cria o elemento da tabela -----//
+  innerHTMLTransaction(transaction) {
+    const CSSClass = transaction.amount > 0 ? 'income' : 'expense'
+
+    const amount = utils.formatCurrency(transaction.amount)
+
+    const html = ` 
+    
+      <td class="description">${transaction.description}</td>
+      <td class="${CSSClass}">${amount}</td>
+      <td class="date">${transaction.date}</td>
+      <td>
+        <img src="../assets/minus.svg" alt="Remover transação" />
+      </td>
+    `
+
+    return html
+  },
+
+  //-----  Atualiza os valores exibidos de entrada, saída e total -----//
+  updateBalance() {
+    document.getElementById('income-display').innerHTML = utils.formatCurrency(
+      Transaction.incomes()
+    )
+
+    document.getElementById('expense-display').innerHTML = utils.formatCurrency(
+      Transaction.expenses()
+    )
+
+    document.getElementById('total-display').innerHTML = utils.formatCurrency(
+      Transaction.total()
+    )
+  }
+}
+
+//----- Utilidades -----//
+const utils = {
+  //-----  Formata os valores da tabela -----//
+  formatCurrency(value) {
+    //-----  Verifica +/- e atribui sinal se necessário -----//
+    const signal = Number(value) < 0 ? '-' : ''
+
+    //-----  Converte valor p/ string e remove tudo que não for número  -----//
+    value = String(value).replace(/\D/g, '')
+
+    //-----  Converte p/ number e divide por cem p/ atribuir casas decimais -----//
+    value = Number(value) / 100
+
+    //-----  Formata para valor em R$ -----//
+    value = value.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    })
+
+    return signal + value
+  }
+}
+
+transactions.forEach(transaction => {
+  DOM.addTransaction(transaction)
+})
+
+DOM.updateBalance()
